4a8216dcd14e9e9a3cdce2c654c17323ac1f6dcd
Bethany Priode <bpriode@gmail.com>
2017-09-27 21:17:15 -0400

    initial commit, can render on screen

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/App.css';
+
+
+//import components and containers
+import BaseLayout from './BaseLayout.js';
+import AccountDetail from '../containers/AccountDetail.js';
+import Transaction from '../containers/Transaction.js';
+import UserDetail from '../containers/UserDetail.js';
+import UserList from '../containers/UserList.js';
+
+class App extends Component {
+  render() {
+    return (
+
+        <div className="main">
+          <div className= "jumbotron">
+            <h1 className= "display-3">BankShot</h1>
+            <p className= "lead">Your world wide banking leader.</p>
+            <hr className= "my-4"/>
+            <p>Quick, fast, and sometimes accurate account results.</p>
+            <p className= "lead">
+              <Link className= "btn btn-primary btn-lg" to="/users" role="button">Check Users</Link>
+              </p>
+            </div>
+        </div>
+
+
+    );
+  }
+}
+
+export default App;
